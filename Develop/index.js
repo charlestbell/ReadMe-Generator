be502a5b502f673229cc2a331a5fc1b4e165ca1a
C.T. Bell <charlestbell@gmail.com>
2021-01-15 20:28:39 -0500

    Adding Example Readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,41 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What languages do you know?",
+    name: "languages",
+  },
+  {
+    type: "input",
+    message: "What is your preferred method of communication?",
+    name: "contactType",
+  },
 ];
 
+inquirer
+  .prompt(questions)
+  .then((response) =>
+    fs.appendFile(
+      "userInfo.txt",
+      `Name ${response.name}\nLanguage ${response.languages}\nPreferred Contact ${response.contactType}`,
+      (err) => (err ? console.error(err) : console.log("Commit logged!"))
+    )
+  );
+
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  writeToFile();
 }
 
 // function call to initialize program
---
+++ Readme_Example.md
@@ -0,0 +1,40 @@
+
+
+/github/license/:user/:repo
+
+# Title <!-- omit in toc -->
+
+### Table of Contents
+
+- [Description](#description)
+- [Installationo](#installationo)
+- [How to use it](#how-to-use-it)
+  - [Credits](#credits)
+  - [Licence](#licence)
+  - [How to contribute](#how-to-contribute)
+  - [Tests](#tests)
+
+## Description
+
+This is what the program does
+
+## Installationo
+
+This is how to install the program
+
+## How to use it
+
+This is how to make the program do the cool shit in the description.
+
+### Credits
+
+Developer: C.T. Bell
+
+### Licence
+Link to licence
+
+### How to contribute
+
+### Tests
+
+
